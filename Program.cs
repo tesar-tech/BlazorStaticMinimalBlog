60c1b626147a2de7a8b28025450655268c439548
tesar-tech <jan@tesar.tech>
2023-11-23 19:54:26 +0100

    adds and uses BlazorStatic package

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using BlazorStatic;
 using BlazorStaticMinimalBlog.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddBlazorStaticService(opt => {
+    opt.IgnoredPathsOnContentCopy.Add("app.css");//pre-build version for tailwind
+}
+).AddBlogService<FrontMatter>();
+
 
 var app = builder.Build();
 
@@ -21,6 +25,9 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>();
+
+app.UseBlog<FrontMatter>();
+app.UseBlazorStaticGenerator(shutdownApp: !app.Environment.IsDevelopment());
 
 app.Run();
 
